1464e7b217862866a4b9bbe18643e7cd4f4a0b9d
jana <janakolesnikova6293@gmail.com>
2024-01-06 23:08:03 +0400

    Registration, sign in, logout

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user_params = params.require(:session)
+
+    user = User.find_by(email: user_params[:email])
+
+    if user.present?
+      session[:user_id] = user.id
+      redirect_to home_path, notice: 'Logged in successfully!'
+    else
+      flash.now[:alert] = 'Wrong email or password!'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    redirect_to home_path, notice: 'Logged out successfully!'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resource :session, only: [:new, :create, :destroy]
+
   resources :users, only: [:new, :create]
 end
